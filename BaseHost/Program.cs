c628846448a1a019103b6bced1ddd5bd22b3327c
jackcu <13801790374@163.com>
2020-02-18 09:00:18 +0800

    测试跑小学应用题

--- BaseHost/Program.cs
+++ BaseHost/Program.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -11,6 +12,60 @@ namespace BaseHost {
 
     public class Program {
         public static async Task Main(string[] args) {
+
+            decimal 跑道 = 1200m;
+            decimal 甲速度 = 10m;
+            decimal 乙速度 = 4m;
+
+            decimal 乙当前耗时 = 0m;
+            decimal 甲当前耗时 = 0m;
+            decimal 甲距离 = 0m;
+            decimal 乙距离 = 0m;
+
+            decimal 甲距离_in_跑道 = 0m;
+            decimal 乙距离_in_跑道 = 0m;
+
+            decimal 乙圈数 = 0m;
+
+            int 圈数 = 1;
+
+            while (true) {
+
+                乙距离 = 乙速度 * 乙当前耗时;
+                甲距离 = 甲速度 * 甲当前耗时;
+
+                if (甲距离 == 跑道) {
+                    甲距离_in_跑道 = 0;
+                    if (圈数 == 1)
+                        圈数++;
+                }
+                else {
+                    甲距离_in_跑道 = 甲距离 % 跑道;
+                }
+
+                if (乙距离 == 跑道) {
+                    乙距离_in_跑道 = 0;
+                    if (圈数 == 1)
+                        圈数++;
+                }
+                else {
+                    乙距离_in_跑道 = 乙距离 % 跑道;
+                }
+
+                Console.WriteLine($"甲当前耗时:{甲当前耗时}秒,乙当前耗时:{乙当前耗时}秒,甲距离:{甲距离},乙距离:{乙距离},乙距离_in_跑道:{乙距离_in_跑道},甲距离_in_跑道:{甲距离_in_跑道}");
+
+                
+
+                if (乙当前耗时 >= 10) {
+                    甲当前耗时++;
+                    乙当前耗时++;
+                }
+                else {
+                    乙当前耗时++;
+                }
+            }
+
+            return;
 
             await Host.CreateDefaultBuilder(args)
                 .ConfigureLogging(logging => logging.AddConsole())
